db1383f8882c4ed0c161f135a92fce0c5ec3b5a5
rdlugs <rojensonlugo@gmail.com>
2021-11-27 00:40:31 +0800

    27/11/2021

--- src/Store.js
+++ src/Store.js
@@ -2,6 +2,4 @@ import { createStore } from "redux"
 import allReducers from "./reducers"
 
 export default createStore(
-    allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+    allReducers)
